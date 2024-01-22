42e11355f29a50712e951327eeb1029e8048748d
fierylion <mawalladaniel2018@gmail.com>
2024-01-22 23:28:40 +0300

    completed second project

---
+++ javascript_projects/sidebar/app.js
@@ -0,0 +1,12 @@
+// const toggleBtn = document.querySelector('.sidebar-toggle');
+// const closeBtn = document.querySelector('.close-btn');
+// const sidebar = document.getElementsByTagName('aside')[0];
+
+// // toggle aside class
+// const toggleAside = () => {
+//     sidebar.classList.toggle('show-sidebar');
+// }
+
+// toggleBtn.addEventListener('click', toggleAside);
+
+// closeBtn.addEventListener('click', ()=> sidebar.classList.remove('show-sidebar'));

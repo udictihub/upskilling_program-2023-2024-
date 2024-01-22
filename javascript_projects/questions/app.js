08f2bc05de62a461c38fd26826618d171b45364c
fierylion <mawalladaniel2018@gmail.com>
2024-01-23 01:04:53 +0300

    commented js on the fourth project

--- javascript_projects/questions/app.js
+++ javascript_projects/questions/app.js
@@ -1,17 +1,17 @@
-const questions = document.querySelectorAll(".question")
-questions.forEach(
-    (qn)=>{
-        //btn
-        const btn = qn.querySelector(".question-btn");
-        btn.addEventListener("click", ()=>{
-            // close other questions
-            questions.forEach((item)=>{
-                if(item !==qn){
-                    item.classList.remove("show-text");
-                }
-            });
-        qn.classList.toggle("show-text");
+// const questions = document.querySelectorAll(".question")
+// questions.forEach(
+//     (qn)=>{
+//         //btn
+//         const btn = qn.querySelector(".question-btn");
+//         btn.addEventListener("click", ()=>{
+//             // close other questions
+//             questions.forEach((item)=>{
+//                 if(item !==qn){
+//                     item.classList.remove("show-text");
+//                 }
+//             });
+//         qn.classList.toggle("show-text");
 
-        })
-    }
-)
+//         })
+//     }
+// )

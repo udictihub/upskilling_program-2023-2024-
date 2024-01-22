01bdde4e6a522ea7e07ef3d376704a30c3fd7a5c
fierylion <mawalladaniel2018@gmail.com>
2024-01-22 22:27:15 +0300

    completed first project

---
+++ javascript_projects/color_flipper/app.js
@@ -0,0 +1,23 @@
+// //required
+// //vars
+// const changeButton = document.getElementById('btn');
+// const colorNameSpan = document.querySelector('.color');
+
+// const getRandomColor= () => {
+//     // get random number from 0 to 255
+//     const randomNumber = ()=> Math.floor(Math.random() * 256);
+//     return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+// }
+
+// const changeColor = () => {
+//     const randomColor = getRandomColor();
+//     document.body.style.backgroundColor = randomColor;
+//     colorNameSpan.textContent = randomColor;
+// }
+
+
+// changeButton.addEventListener('click', changeColor);
+
+// //initially
+// changeColor();
+

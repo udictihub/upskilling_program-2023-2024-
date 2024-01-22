2d79d718b06a38372dbb075246b7eac92a108019
fierylion <mawalladaniel2018@gmail.com>
2024-01-23 00:05:24 +0300

    finished third project

---
+++ javascript_projects/modal/app.js
@@ -0,0 +1,8 @@
+// const openBtn = document.querySelector('.open-btn');
+// const closeBtn = document.querySelector('.close-btn');
+
+// const modal = document.querySelector('.modal-overlay');
+
+
+// openBtn.addEventListener('click', ()=> modal.classList.add('open-modal'));
+// closeBtn.addEventListener('click', ()=> modal.classList.remove('open-modal'));
